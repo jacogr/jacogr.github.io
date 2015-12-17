1e5b51cee7b9b40632bff27cda1cf330b1862d0a
jacogr <j@cqscloud.com>
2015-12-17 12:00:45 +0200

    add level, faster animation

--- cv/client.js
+++ cv/client.js
@@ -76,7 +76,8 @@ service('Data', function () {
     type: 'employed', 
     company: 'CQS Technology Holdings', 
     location: 'Johannesburg, ZA', 
-    position: 'CTO' }, 
+    position: 'CTO', 
+    level: 'executive' }, 
 
   { 
     start: { year: 2011, month: 7 }, 
@@ -84,7 +85,8 @@ service('Data', function () {
     type: 'founded', 
     company: 'Tabula', 
     location: 'Johannesburg, ZA', 
-    position: 'CTO & Founder' }, 
+    position: 'CTO & Founder', 
+    level: 'executive' }, 
 
   { 
     start: { year: 2008, month: 9 }, 
@@ -92,7 +94,8 @@ service('Data', function () {
     type: 'employed', 
     company: 'Cura Software Solutions', 
     location: 'Johannesburg, ZA & Hyderabad, IN', 
-    position: 'CTO' }, 
+    position: 'CTO', 
+    level: 'executive' }, 
 
   { 
     start: { year: 2007, month: 7 }, 
@@ -100,7 +103,8 @@ service('Data', function () {
     type: 'founded', 
     company: 'Various', 
     location: 'Johannesburg, ZA', 
-    position: 'Consultant' }, 
+    position: 'Consultant', 
+    level: 'management' }, 
 
   { 
     start: { year: 2003, month: 4 }, 
@@ -108,7 +112,8 @@ service('Data', function () {
     type: 'employed', 
     company: 'Discovery Health', 
     location: 'Johannesburg, ZA', 
-    position: 'Senior Architect' }, 
+    position: 'Senior Architect', 
+    level: 'management' }, 
 
   { 
     start: { year: 1999, month: 4 }, 
@@ -116,7 +121,8 @@ service('Data', function () {
     type: 'employed', 
     company: 'healthbridge', 
     location: 'Johannesburg, ZA', 
-    position: 'Technical Manager' }, 
+    position: 'Technical Manager', 
+    level: 'management' }, 
 
   { 
     start: { year: 1998, month: 10 }, 
@@ -125,6 +131,7 @@ service('Data', function () {
     company: 'Internet Solutions', 
     location: 'Johannesburg, ZA', 
     position: 'Senior Developer', 
+    level: 'staff', 
     description: 'Internet Solution is a leading ISP in ZA. In addition to the traditional ISP business, they also did project-based work for clients with Internet deployments and website development.\n* Team Lead for the Dimension Data Healthcare project\n* Design and development of a real-time messaging switch for the Dimension Data Healthcare initiative\n* Dimension Data Healthcare led to the formation of HealthBridge' }, 
 
 
@@ -137,6 +144,7 @@ service('Data', function () {
     company: 'Crusader Systems', 
     location: 'Stellenbosch & Pretoria, ZA', 
     position: 'Developer', 
+    level: 'staff', 
     description: 'Crusader System is an Artificial Intelligence consulting company with a large focus on process optimization in the mining industry.\n* Development of the first-generation AI modeling tool, ModelGen\n* Consulting to Richard’s Bay Mineral and Karee Platinum mines on process optimization using the ModelGen toolset' }, 
 
 
@@ -148,6 +156,7 @@ service('Data', function () {
     company: 'Grinaker Electronics', 
     location: 'Tokai, ZA', 
     position: 'Developer', 
+    level: 'staff', 
     description: 'Grinaker Electronics is a project-based company in the commercial sector (vehicle tracking) along with the implementation of projects for defense agencies such as ARMSCOR.\n* Development and maintenance of the G-Track vehicle tracking system\n* Development of a full ISO networking implementation (Physical to Application layers) for ARMSCOR as part of a new radio communications platform' }, 
 
 
@@ -158,4 +167,5 @@ service('Data', function () {
     type: 'education', 
     company: 'University of Stellenbosch', 
     location: 'Stellenbosh, ZA', 
-    position: 'B.Eng (Electronic)' }];});
+    position: 'B.Eng (Electronic)', 
+    level: 'student' }];});
